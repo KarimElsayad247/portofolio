9a09f3f6f59ec3f4fe4de735f9cee0d6ac340099
KarimElsayad247 <karimelsayad1999@gmail.com>
2021-10-14 08:06:53 +0200

    Fill projects section dynamically
    Using data entered in projects.js, index.html is kept consice, and
    projects are filled dynamically.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,26 +1,64 @@
-/*
-                <li class="project-card">
-                    <div class="project-discription">
-                        <h3>Landing page</h3>
-                        <p>A simple landing page using HTML/CSS</p>
-                    </div>
-                    <div class="card-links">
-                        <div id="repo-link" class="link">
-                            <a href="https://github.com/KarimElsayad247/landing-page">
-                                <img src="images/github.png" alt="github logo">Github
-                            </a>
-                        </div>
-                        <div id="live-link" class="link">
-                            <a href="http://karimelsayad.me/landing-page/">
-                                <img src="images/antenna.png" alt="antenna icon">Live!
-                            </a>
-                        </div>
-                    </div>
-                </li>s
-*/
-
 function fillProjects() {
-    console.log(projects);
+    projects.forEach(project => {
+        let listItem = document.createElement('li');
+        listItem.className = "project-card";
+        
+        let projectDescriptionDiv = document.createElement('div');
+        projectDescriptionDiv.className = "project-discription";
+
+        let cardLinksDiv = document.createElement('div');
+        cardLinksDiv.className = "card-links";
+
+        let repoLinkDiv = document.createElement('div');
+        repoLinkDiv.className = "link";
+        repoLinkDiv.id = "repo-link";
+
+        let liveLinkDiv = document.createElement('div');
+        liveLinkDiv.className = "link";
+        liveLinkDiv.id = "live-link";
+
+        let projectTitle = document.createElement('h3');
+        projectTitle.textContent = project.title;
+
+        let projectDescription = document.createElement('p');
+        projectDescription.textContent = project.description;
+
+        listItem.appendChild(projectDescriptionDiv);
+        listItem.appendChild(cardLinksDiv);
+
+        projectDescriptionDiv.appendChild(projectTitle);
+        projectDescriptionDiv.appendChild(projectDescription);
+
+
+        let githubLink = document.createElement('a');
+        githubLink.href = project.github;   
+
+        let githubLogo = document.createElement('img');
+        githubLogo.src = "../images/github.png";
+        githubLogo.alt = "github logo";
+        
+        let liveLink = document.createElement('a');
+        liveLink.href = project.live;   
+
+        let liveLogo = document.createElement('img');
+        liveLogo.src = "../images/antenna.png";
+        liveLogo.alt = "antenna icon";
+
+        githubLink.appendChild(githubLogo);
+        githubLink.appendChild(document.createTextNode("Github"));
+        liveLink.appendChild(liveLogo);
+        liveLink.appendChild(document.createTextNode("Live!"));
+        repoLinkDiv.appendChild(githubLink);
+        liveLinkDiv.appendChild(liveLink);
+
+        
+        cardLinksDiv.appendChild(repoLinkDiv);
+        cardLinksDiv.appendChild(liveLinkDiv);
+
+        var list = document.getElementsByClassName("projects-container");
+        list[0].appendChild(listItem);
+
+    });
 
 }
 
