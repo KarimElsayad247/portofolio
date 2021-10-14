ae8bbe5aa6cc231a415bac083149c134ecbd31ab
KarimElsayad247 <karimelsayad1999@gmail.com>
2021-10-14 10:54:53 +0200

    Add another project

--- data/projects.js
+++ data/projects.js
@@ -13,4 +13,11 @@ projects = [
         "github": "https://github.com/KarimElsayad247/course-browse",
         "live": "http://portfolio.karimelsayad.me/course-browse"
 
-    }]
+    },
+    {
+        "title": "Huffman Compression",
+        "description": "Implement the Huffman compression algorithms for an algorithms course. This C++ program is practical and can be readily used to compress text files.",
+        "github": "https://github.com/KarimElsayad247/huffman-compression",
+        "live": "none"
+    }
+]
